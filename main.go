522d96cf84dd65b9fdc194029a53386220cee761
mohammed266392 <mohammed.raki26@gmail.com>
2025-07-19 20:44:55 +0200

    authorisation cors

--- main.go
+++ main.go
@@ -138,7 +138,7 @@ func LoginUsersHandler(w http.ResponseWriter, r *http.Request) {
 func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Autorise les requêtes depuis ton frontend Angular
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		w.Header().Set("Access-Control-Allow-Origin", "http://mowam-solution.de")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
